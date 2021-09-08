6944da97be18fc4b6734ac0b051d8d599adf7bda
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-09-07 22:03:07 -0300

    Commit Final

---
+++ Impressora Bematech/src/javaapplication1/teste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.awt.List;
+import java.util.ArrayList;
+import javax.print.DocFlavor;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+
+/**
+ *
+ * @author Murilo
+ */
+public class teste {
+    
+}
